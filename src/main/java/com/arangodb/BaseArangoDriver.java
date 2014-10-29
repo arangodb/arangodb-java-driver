9e125dc77b1627a88b1767a128c834ecf0610249
gschwab <g.schwab@triagens.de>
2014-10-29 11:22:12 +0100

    code style

--- src/main/java/com/arangodb/BaseArangoDriver.java
+++ src/main/java/com/arangodb/BaseArangoDriver.java
@@ -21,7 +21,12 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-import com.arangodb.entity.*;
+import com.arangodb.entity.BaseEntity;
+import com.arangodb.entity.EntityDeserializers;
+import com.arangodb.entity.EntityFactory;
+import com.arangodb.entity.KeyValueEntity;
+import com.arangodb.entity.ReplicationDumpHeader;
+import com.arangodb.entity.StreamEntity;
 import com.arangodb.entity.marker.MissingInstanceCreater;
 import com.arangodb.http.HttpResponseEntity;
 import com.arangodb.util.DateUtils;
@@ -36,15 +41,16 @@ public abstract class BaseArangoDriver {
 
   private static final Pattern databaseNamePattern = Pattern.compile("^[a-zA-Z][a-zA-Z0-9\\-_]{0,63}$");
 
-//  protected String createDocumentHandle(long collectionId, long documentId) {
-//    // validateCollectionNameは不要
-//    return collectionId + "/" + documentId;
-//  }
-//
-//  protected String createDocumentHandle(String collectionName, long documentId) throws ArangoException {
-//    validateCollectionName(collectionName);
-//    return collectionName + "/" + documentId;
-//  }
+  // protected String createDocumentHandle(long collectionId, long documentId) {
+  // // validateCollectionNameは不要
+  // return collectionId + "/" + documentId;
+  // }
+  //
+  // protected String createDocumentHandle(String collectionName, long
+  // documentId) throws ArangoException {
+  // validateCollectionName(collectionName);
+  // return collectionName + "/" + documentId;
+  // }
 
   protected String createDocumentHandle(long collectionId, String documentKey) {
     return collectionId + "/" + documentKey;
@@ -55,19 +61,18 @@ public abstract class BaseArangoDriver {
     return collectionName + "/" + documentKey;
   }
 
-  
   protected void validateCollectionName(String name) throws ArangoException {
     if (name.indexOf('/') != -1) {
       throw new ArangoException("does not allow '/' in name.");
     }
   }
-  
+
   protected void validateDocumentHandle(String documentHandle) throws ArangoException {
     int pos = documentHandle.indexOf('/');
     if (pos > 0) {
       String collectionName = documentHandle.substring(0, pos);
       String documentKey = documentHandle.substring(pos + 1);
-      
+
       validateCollectionName(collectionName);
       if (collectionName.length() != 0 && documentKey.length() != 0) {
         return;
@@ -75,12 +80,13 @@ public abstract class BaseArangoDriver {
     }
     throw new ArangoException("invalid format documentHandle:" + documentHandle);
   }
-  
+
   /**
    * @param database
    * @param allowNull
    * @throws ArangoException
-   * @see http://www.arangodb.org/manuals/current/NamingConventions.html#DatabaseNames
+   * @see http
+   *      ://www.arangodb.org/manuals/current/NamingConventions.html#DatabaseNames
    */
   protected void validateDatabaseName(String database, boolean allowNull) throws ArangoException {
     boolean valid = false;
@@ -95,83 +101,87 @@ public abstract class BaseArangoDriver {
       throw new ArangoException("invalid format database:" + database);
     }
   }
-  
+
   protected void setKeyValueHeader(HttpResponseEntity res, KeyValueEntity entity) throws ArangoException {
-    
+
     Map<String, String> headers = res.getHeaders();
-    
+
     try {
       String strCreated = headers.get("x-voc-created");
       if (strCreated != null) {
         entity.setCreated(DateUtils.parse(strCreated, "yyyy-MM-dd'T'HH:mm:ss'Z'"));
       }
-      
+
       String strExpires = headers.get("x-voc-expires");
       if (strExpires != null) {
         entity.setExpires(DateUtils.parse(strExpires, "yyyy-MM-dd'T'HH:mm:ss'Z'"));
       }
-      
+
       String strExtened = headers.get("x-voc-extended");
       if (strExtened != null) {
         Map<String, Object> attributes = EntityFactory.createEntity(strExtened, Map.class);
         entity.setAttributes(attributes);
       }
-      
+
     } catch (ParseException e) {
       throw new ArangoException(e);
     }
-    
+
   }
-  
+
   protected ReplicationDumpHeader toReplicationDumpHeader(HttpResponseEntity res) {
     ReplicationDumpHeader header = new ReplicationDumpHeader();
-    
+
     Map<String, String> headerMap = res.getHeaders();
     String value;
-    
+
     value = headerMap.get("x-arango-replication-active");
     if (value != null) {
       header.setActive(Boolean.parseBoolean(value));
     }
-    
+
     value = headerMap.get("x-arango-replication-lastincluded");
     if (value != null) {
       header.setLastincluded(Long.parseLong(value));
     }
-    
+
     value = headerMap.get("x-arango-replication-lasttick");
     if (value != null) {
       header.setLasttick(Long.parseLong(value));
     }
-    
+
     value = headerMap.get("x-arango-replication-checkmore");
     if (value != null) {
       header.setCheckmore(Boolean.parseBoolean(value));
     }
-    
+
     return header;
   }
-  
+
   /**
-   * HTTPレスポンスから指定した型へ変換する。
-   * レスポンスがエラーであるかを確認して、エラーの場合は例外を投げる。
+   * HTTPレスポンスから指定した型へ変換する。 レスポンスがエラーであるかを確認して、エラーの場合は例外を投げる。
+   * 
    * @param res
    * @param type
    * @param validate
    * @return
    * @throws ArangoException
    */
-  protected <T extends BaseEntity> T createEntity(HttpResponseEntity res, Class<? extends BaseEntity> clazz, Class<?>[] pclazz, boolean validate) throws ArangoException {
+  protected <T extends BaseEntity> T createEntity(
+    HttpResponseEntity res,
+    Class<? extends BaseEntity> clazz,
+    Class<?>[] pclazz,
+    boolean validate) throws ArangoException {
     if (res == null) {
       return null;
     }
     try {
       EntityDeserializers.setParameterized(pclazz);
-      
+
       T entity = createEntityImpl(res, clazz);
       if (entity == null) {
         Class<?> c = MissingInstanceCreater.getMissingClass(clazz);
-         entity = ReflectionUtils.newInstance(c);
+        entity = ReflectionUtils.newInstance(c);
       } else if (res.isBatchRepsonse()) {
         try {
           entity = (T) clazz.newInstance();
@@ -202,7 +212,10 @@ public abstract class BaseArangoDriver {
     return createEntity(res, clazz, null, true);
   }
 
-  protected <T extends BaseEntity> T createEntity(HttpResponseEntity res, Class<? extends BaseEntity> clazz, Class<?>... pclazz) throws ArangoException {
+  protected <T extends BaseEntity> T createEntity(
+    HttpResponseEntity res,
+    Class<? extends BaseEntity> clazz,
+    Class<?>... pclazz) throws ArangoException {
     return createEntity(res, clazz, pclazz, true);
   }
 
@@ -217,20 +230,20 @@ public abstract class BaseArangoDriver {
       }
     }
   }
-  
+
   protected void validate(HttpResponseEntity res, BaseEntity entity) throws ArangoException {
-    
+
     if (entity != null) {
       if (entity.isError()) {
         throw new ArangoException(entity);
       }
     }
-    
+
     // Custom Error
     if (res.getStatusCode() >= 400) {
-      
+
       if (res.isTextResponse()) {
-        //entity.setErrorNumber(0);
+        // entity.setErrorNumber(0);
         entity.setErrorNumber(res.getStatusCode());
         entity.setErrorMessage(res.getText());
       } else {
@@ -239,19 +252,19 @@ public abstract class BaseArangoDriver {
       }
 
       switch (res.getStatusCode()) {
-      case 401:
-        entity.setErrorMessage("Unauthorized");
-        break;
-      case 403:
-        entity.setErrorMessage("Forbidden");
-        break;
-      default:
+        case 401 :
+          entity.setErrorMessage("Unauthorized");
+          break;
+        case 403 :
+          entity.setErrorMessage("Forbidden");
+          break;
+        default :
       }
 
       throw new ArangoException(entity);
     }
   }
-  
+
   protected <T> T createEntityImpl(HttpResponseEntity res, Class<?> type) throws ArangoException {
     if (res.isJsonResponse()) {
       T entity = EntityFactory.createEntity(res.getText(), type);
@@ -261,26 +274,27 @@ public abstract class BaseArangoDriver {
       return (T) new StreamEntity(res.getStream());
     }
     return null;
-    //throw new IllegalStateException("unknown response content-type:" + res.getContentType());
+    // throw new IllegalStateException("unknown response content-type:" +
+    // res.getContentType());
   }
-  
-  protected String createEndpointUrl(String baseUrl, String database, Object...paths) throws ArangoException {
+
+  protected String createEndpointUrl(String baseUrl, String database, Object... paths) throws ArangoException {
 
     // FIXME: Very very foolish implement.
-    
+
     ArrayList<String> list = new ArrayList<String>();
-    
+
     if (database != null) {
       validateDatabaseName(database, false);
       list.add("_db");
       list.add(database);
     }
-    for (Object path: paths) {
+    for (Object path : paths) {
       if (path != null) {
         list.add(path.toString());
       }
     }
     return baseUrl + StringUtils.join(false, list);
   }
-  
+
 }
--- src/test/java/com/arangodb/ArangoDriverGraphVertexReplaceTest.java
+++ src/test/java/com/arangodb/ArangoDriverGraphVertexReplaceTest.java
@@ -16,14 +16,14 @@
 
 package com.arangodb;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-import com.arangodb.ArangoConfigure;
-import com.arangodb.ArangoDriver;
-import com.arangodb.ArangoException;
 import com.arangodb.entity.DocumentEntity;
 import com.arangodb.entity.GraphEntity;
 
@@ -36,190 +36,234 @@ public class ArangoDriverGraphVertexReplaceTest extends BaseGraphTest {
   public ArangoDriverGraphVertexReplaceTest(ArangoConfigure configure, ArangoDriver driver) {
     super(configure, driver);
   }
-  
+
   @Test
   public void test_vertex_replace() throws ArangoException {
 
     // create graph
-    GraphEntity g1 = driver.createGraph("g1","v1", "e1", null);
+    GraphEntity g1 = driver.createGraph("g1", "v1", "e1", null);
     // create vertex
     DocumentEntity<TestComplexEntity01> v1 = driver.createVertex("g1", new TestComplexEntity01("xxx", "yyy", 10), null);
     // check exists vertex
-    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex("g1", v1.getDocumentKey(), TestComplexEntity01.class, 
-        null, null, null);
+    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex(
+      "g1",
+      v1.getDocumentKey(),
+      TestComplexEntity01.class,
+      null,
+      null,
+      null);
     assertThat(vertex.getCode(), is(200));
-    
+
     // replace
-    DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", v1.getDocumentKey(), new TestComplexEntity02(1,2,3));
+    DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex(
+      "g1",
+      v1.getDocumentKey(),
+      new TestComplexEntity02(1, 2, 3));
     assertThat(updatedVertex.getCode(), is(202));
     assertThat(updatedVertex.isError(), is(false));
-    
+
     assertThat(updatedVertex.getDocumentHandle(), is(v1.getDocumentHandle()));
     assertThat(updatedVertex.getDocumentRevision(), is(not(v1.getDocumentRevision())));
     assertThat(updatedVertex.getDocumentRevision(), is(not(0L)));
     assertThat(updatedVertex.getDocumentKey(), is(v1.getDocumentKey()));
-    
+
     assertThat(updatedVertex.getEntity().getX(), is(1));
     assertThat(updatedVertex.getEntity().getY(), is(2));
     assertThat(updatedVertex.getEntity().getZ(), is(3));
-    
+
     // check count
     assertThat(driver.getCollectionCount("v1").getCount(), is(1L));
-    
+
   }
 
   @Test
   public void test_vertex_replace_graph_not_found() throws ArangoException {
-    
+
     try {
-      DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", "key1", new TestComplexEntity02(1,2,3));
+      DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", "key1", new TestComplexEntity02(1,
+          2, 3));
       fail();
     } catch (ArangoException e) {
       assertThat(e.getCode(), is(404));
       assertThat(e.getErrorNumber(), is(1901));
       assertThat(e.getErrorMessage(), startsWith("no graph named"));
     }
-    
+
   }
 
   @Test
   public void test_vertex_replace_vertex_not_found() throws ArangoException {
 
     // create graph
-    GraphEntity g1 = driver.createGraph("g1","v1", "e1", null);
+    GraphEntity g1 = driver.createGraph("g1", "v1", "e1", null);
     // replace
     try {
-      DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", "key1", new TestComplexEntity02(1,2,3));
+      DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", "key1", new TestComplexEntity02(1,
+          2, 3));
       fail();
     } catch (ArangoException e) {
       assertThat(e.getCode(), is(404));
       assertThat(e.getErrorNumber(), is(1905));
       assertThat(e.getErrorMessage(), startsWith("no vertex found for"));
     }
-    
+
   }
-  
+
   @Test
   public void test_vertex_replace_rev_eq() throws ArangoException {
 
     // create graph
-    GraphEntity g1 = driver.createGraph("g1","v1", "e1", null);
+    GraphEntity g1 = driver.createGraph("g1", "v1", "e1", null);
     // create vertex
     DocumentEntity<TestComplexEntity01> v1 = driver.createVertex("g1", new TestComplexEntity01("xxx", "yyy", 10), null);
     // check exists vertex
-    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex("g1", v1.getDocumentKey(), TestComplexEntity01.class, 
-        null, null, null);
+    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex(
+      "g1",
+      v1.getDocumentKey(),
+      TestComplexEntity01.class,
+      null,
+      null,
+      null);
     assertThat(vertex.getCode(), is(200));
-    
-    
+
     // replace
     Long rev = vertex.getDocumentRevision();
-    DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", v1.getDocumentKey(), new TestComplexEntity02(1,2,3), true,
-        rev, null);
+    DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex(
+      "g1",
+      v1.getDocumentKey(),
+      new TestComplexEntity02(1, 2, 3),
+      true,
+      rev,
+      null);
     assertThat(updatedVertex.getCode(), is(201));
     assertThat(updatedVertex.isError(), is(false));
-    
+
     assertThat(updatedVertex.getDocumentHandle(), is(v1.getDocumentHandle()));
     assertThat(updatedVertex.getDocumentRevision(), is(not(v1.getDocumentRevision())));
     assertThat(updatedVertex.getDocumentRevision(), is(not(0L)));
     assertThat(updatedVertex.getDocumentKey(), is(v1.getDocumentKey()));
-    
+
     assertThat(updatedVertex.getEntity().getX(), is(1));
     assertThat(updatedVertex.getEntity().getY(), is(2));
     assertThat(updatedVertex.getEntity().getZ(), is(3));
-    
+
     // check count
     assertThat(driver.getCollectionCount("v1").getCount(), is(1L));
-    
+
   }
 
-  
   @Test
   public void test_vertex_replace_rev_ne() throws ArangoException {
 
     // create graph
-    GraphEntity g1 = driver.createGraph("g1","v1", "e1", null);
+    GraphEntity g1 = driver.createGraph("g1", "v1", "e1", null);
     // create vertex
     DocumentEntity<TestComplexEntity01> v1 = driver.createVertex("g1", new TestComplexEntity01("xxx", "yyy", 10), null);
     // check exists vertex
-    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex("g1", v1.getDocumentKey(), TestComplexEntity01.class, 
-        null, null, null);
+    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex(
+      "g1",
+      v1.getDocumentKey(),
+      TestComplexEntity01.class,
+      null,
+      null,
+      null);
     assertThat(vertex.getCode(), is(200));
-    
-    
+
     // replace
     try {
       Long rev = 1L;
-      DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", v1.getDocumentKey(), new TestComplexEntity02(1,2,3), true,
-          rev, null);
+      DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex(
+        "g1",
+        v1.getDocumentKey(),
+        new TestComplexEntity02(1, 2, 3),
+        true,
+        rev,
+        null);
       fail();
     } catch (ArangoException e) {
       assertThat(e.getCode(), is(412));
       assertThat(e.getErrorNumber(), is(1903));
       assertThat(e.getErrorMessage(), is("wrong revision"));
     }
-    
-  }
 
+  }
 
   @Test
   public void test_vertex_replace_ifmatch_eq() throws ArangoException {
 
     // create graph
-    GraphEntity g1 = driver.createGraph("g1","v1", "e1", null);
+    GraphEntity g1 = driver.createGraph("g1", "v1", "e1", null);
     // create vertex
     DocumentEntity<TestComplexEntity01> v1 = driver.createVertex("g1", new TestComplexEntity01("xxx", "yyy", 10), null);
     // check exists vertex
-    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex("g1", v1.getDocumentKey(), TestComplexEntity01.class, 
-        null, null, null);
+    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex(
+      "g1",
+      v1.getDocumentKey(),
+      TestComplexEntity01.class,
+      null,
+      null,
+      null);
     assertThat(vertex.getCode(), is(200));
-    
-    
+
     // replace
     Long rev = vertex.getDocumentRevision();
-    DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", v1.getDocumentKey(), new TestComplexEntity02(1,2,3), true,
-        null, rev);
+    DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex(
+      "g1",
+      v1.getDocumentKey(),
+      new TestComplexEntity02(1, 2, 3),
+      true,
+      null,
+      rev);
     assertThat(updatedVertex.getCode(), is(201));
     assertThat(updatedVertex.isError(), is(false));
-    
+
     assertThat(updatedVertex.getDocumentHandle(), is(v1.getDocumentHandle()));
     assertThat(updatedVertex.getDocumentRevision(), is(not(v1.getDocumentRevision())));
     assertThat(updatedVertex.getDocumentRevision(), is(not(0L)));
     assertThat(updatedVertex.getDocumentKey(), is(v1.getDocumentKey()));
-    
+
     assertThat(updatedVertex.getEntity().getX(), is(1));
     assertThat(updatedVertex.getEntity().getY(), is(2));
     assertThat(updatedVertex.getEntity().getZ(), is(3));
-    
+
     // check count
     assertThat(driver.getCollectionCount("v1").getCount(), is(1L));
-    
+
   }
 
   @Test
   public void test_vertex_replace_ifmatch_ne() throws ArangoException {
 
     // create graph
-    GraphEntity g1 = driver.createGraph("g1","v1", "e1", null);
+    GraphEntity g1 = driver.createGraph("g1", "v1", "e1", null);
     // create vertex
     DocumentEntity<TestComplexEntity01> v1 = driver.createVertex("g1", new TestComplexEntity01("xxx", "yyy", 10), null);
     // check exists vertex
-    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex("g1", v1.getDocumentKey(), TestComplexEntity01.class, 
-        null, null, null);
+    DocumentEntity<TestComplexEntity01> vertex = driver.getVertex(
+      "g1",
+      v1.getDocumentKey(),
+      TestComplexEntity01.class,
+      null,
+      null,
+      null);
     assertThat(vertex.getCode(), is(200));
-    
-    
+
     // replace
     try {
       Long rev = 1L;
-      DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex("g1", v1.getDocumentKey(), new TestComplexEntity02(1,2,3), true,
-          null, rev);
+      DocumentEntity<TestComplexEntity02> updatedVertex = driver.replaceVertex(
+        "g1",
+        v1.getDocumentKey(),
+        new TestComplexEntity02(1, 2, 3),
+        true,
+        null,
+        rev);
       fail();
     } catch (ArangoException e) {
       assertThat(e.getCode(), is(412));
       assertThat(e.getErrorNumber(), is(1903));
       assertThat(e.getErrorMessage(), is("wrong revision"));
     }
-    
+
   }
 }
