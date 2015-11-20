6006c5ce684336973b26cbf39cd7022410185577
a-brandt <a.brandt@triagens.de>
2015-11-20 15:48:11 +0100

    changed version to 2.7.1-SNAPSHOT

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -8,8 +8,8 @@ NAME=ArangoDB-$VERSION
 
 if [ ! -d "$DIR/$NAME" ]; then
   # download ArangoDB
-  echo "wget http://www.arangodb.org/repositories/travisCI/$NAME.tar.gz"
-  wget http://www.arangodb.org/repositories/travisCI/$NAME.tar.gz
+  echo "wget https://www.arangodb.org/repositories/travisCI/$NAME.tar.gz"
+  wget https://www.arangodb.org/repositories/travisCI/$NAME.tar.gz
   echo "tar zxf $NAME.tar.gz"
   tar zvxf $NAME.tar.gz
 fi
