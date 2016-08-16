93a20ffa13712088d617ca2cac86a272882b448a
a-brandt <a.brandt@triagens.de>
2016-08-16 09:44:48 +0200

    added a test

--- src/test/java/com/arangodb/SimpleApiTest.java
+++ src/test/java/com/arangodb/SimpleApiTest.java
@@ -4,13 +4,12 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-import java.util.Map;
-
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.arangodb.internal.ArangoDBConstants;
+import com.arangodb.velocypack.VPackSlice;
 
 /**
  * @author Mark - mark at arangodb.com
@@ -22,14 +21,28 @@ public class SimpleApiTest {
 
 	@Test
 	public void simplyAnyTest() {
-		Map myMap = null;
+		VPackSlice myMap = null;
 		try {
 			final ArangoDB arangoDB = new ArangoDB.Builder().build();
-			myMap = arangoDB.db().collection("_users").simpleAnyDocument(Map.class).execute();
+			myMap = arangoDB.db().collection("_users").simpleAnyDocument(VPackSlice.class).execute();
 		} catch (ArangoDBException ex) {
 			LOGGER.error(ArangoDBConstants.PATH_API_SIMPLE_ANY + " failed", ex);
 		}
 		assertThat(myMap, is(notNullValue()));
 	}
 
+	@Test
+	public void simplyAnyManyTest() {
+		VPackSlice myMap = null;
+		try {
+			final ArangoDB arangoDB = new ArangoDB.Builder().build();
+			for (int i = 0; i < 100; i++) {
+				myMap = null;
+				myMap = arangoDB.db().collection("_users").simpleAnyDocument(VPackSlice.class).execute();
+			}
+		} catch (ArangoDBException ex) {
+			LOGGER.error(ArangoDBConstants.PATH_API_SIMPLE_ANY + " failed", ex);
+		}
+		assertThat(myMap, is(notNullValue()));
+	}
 }
