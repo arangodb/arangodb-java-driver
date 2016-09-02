bdb017aa7e93eca8d29d322d66cb0b05a86a29f4
Mark <mark@arangodb.com>
2016-09-02 10:37:57 +0200

    wut

--- src/main/java/com/arangodb/internal/net/velocystream/Message.java
+++ src/main/java/com/arangodb/internal/net/velocystream/Message.java
@@ -1,7 +1,6 @@
 package com.arangodb.internal.net.velocystream;
 
 import java.nio.BufferUnderflowException;
-import java.nio.ByteBuffer;
 import java.util.Optional;
 
 import com.arangodb.velocypack.VPackSlice;
@@ -16,8 +15,7 @@ public class Message {
 	private final VPackSlice head;
 	private final Optional<VPackSlice> body;
 
-	public Message(final long id, final byte[] chunkBuffer)
-			throws BufferUnderflowException, IndexOutOfBoundsException {
+	public Message(final long id, final byte[] chunkBuffer) throws BufferUnderflowException, IndexOutOfBoundsException {
 		super();
 		this.id = id;
 		head = new VPackSlice(chunkBuffer);
