9c8b27507d9aad27d0525afe85e3e22f0e4e7902
Mark <mark@arangodb.com>
2016-09-16 10:52:13 +0200

    added disclaimer

--- src/main/java/com/arangodb/velocypack/internal/util/ObjectArrayUtil.java
+++ src/main/java/com/arangodb/velocypack/internal/util/ObjectArrayUtil.java
@@ -1,3 +1,23 @@
+/*
+ * DISCLAIMER
+ *
+ * Copyright 2016 ArangoDB GmbH, Cologne, Germany
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright holder is ArangoDB GmbH, Cologne, Germany
+ */
+
 package com.arangodb.velocypack.internal.util;
 
 import java.util.HashMap;
@@ -12,7 +32,7 @@ public class ObjectArrayUtil {
 	private static final Map<Byte, Integer> FIRST_SUB_MAP;
 
 	static {
-		FIRST_SUB_MAP = new HashMap<Byte, Integer>();
+		FIRST_SUB_MAP = new HashMap<>();
 
 		FIRST_SUB_MAP.put((byte) 0x00, 0); // None
 		FIRST_SUB_MAP.put((byte) 0x01, 1); // empty array
@@ -41,7 +61,7 @@ public class ObjectArrayUtil {
 
 	private static final Map<Byte, Integer> OFFSET_SIZE;
 	static {
-		OFFSET_SIZE = new HashMap<Byte, Integer>();
+		OFFSET_SIZE = new HashMap<>();
 
 		OFFSET_SIZE.put((byte) 0x00, 0); // None
 		OFFSET_SIZE.put((byte) 0x01, 1); // empty array
