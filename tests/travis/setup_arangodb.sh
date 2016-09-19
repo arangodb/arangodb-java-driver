b482f79ef13b9848fc808ed9fe9d3896ea45742a
a-brandt <a.brandt@triagens.de>
2016-09-19 14:12:46 +0200

    added vpp+ssl on port 8530

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -30,8 +30,7 @@ ${ARANGOD} \
     --database.directory ${TMP_DIR} \
     --configuration none \
     --server.endpoint vpp+tcp://127.0.0.1:8529 \
-    --server.endpoint tcp://127.0.0.1:8531 \
-    --server.endpoint ssl://127.0.0.1:8530 \
+    --server.endpoint vpp+ssl://127.0.0.1:8530 \
     --ssl.keyfile ./server.pem \
     --javascript.app-path ${ARANGODB_DIR}/js/apps \
     --javascript.startup-directory ${ARANGODB_DIR}/js \
