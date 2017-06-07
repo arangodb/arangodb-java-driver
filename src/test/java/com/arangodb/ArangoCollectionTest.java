88ca3c20b5884f9dc1654b574e526f1c48ac7a0a
mpv1989 <mark@arangodb.com>
2017-06-07 16:59:01 +0200

    Fix tests for cluster

--- src/test/java/com/arangodb/ArangoCollectionTest.java
+++ src/test/java/com/arangodb/ArangoCollectionTest.java
@@ -59,6 +59,7 @@ import com.arangodb.entity.DocumentUpdateEntity;
 import com.arangodb.entity.IndexEntity;
 import com.arangodb.entity.IndexType;
 import com.arangodb.entity.MultiDocumentEntity;
+import com.arangodb.entity.ServerRole;
 import com.arangodb.model.CollectionCreateOptions;
 import com.arangodb.model.CollectionPropertiesOptions;
 import com.arangodb.model.DocumentCreateOptions;
@@ -745,7 +746,9 @@ public class ArangoCollectionTest extends BaseTest {
 		assertThat(indexResult.getId(), startsWith(COLLECTION_NAME));
 		assertThat(indexResult.getIsNewlyCreated(), is(true));
 		assertThat(indexResult.getMinLength(), is(nullValue()));
-		assertThat(indexResult.getSelectivityEstimate(), is(1));
+		if (arangoDB.getRole() == ServerRole.SINGLE) {
+			assertThat(indexResult.getSelectivityEstimate(), is(1));
+		}
 		assertThat(indexResult.getSparse(), is(false));
 		assertThat(indexResult.getType(), is(IndexType.hash));
 		assertThat(indexResult.getUnique(), is(false));
