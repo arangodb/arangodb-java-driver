f7e747bc2e54bf9fcfe44f1e33108e6319c945c5
mpv1989 <mark@arangodb.com>
2017-06-30 15:23:29 +0200

    Fix travis nightly path

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -8,8 +8,8 @@ NAME=ArangoDB-$VERSION
 
 if [ ! -d "$DIR/$NAME" ]; then
   # download ArangoDB
-  echo "wget https://www.arangodb.com/repositories/nightly/travisCI/$NAME.tar.gz"
-  wget https://www.arangodb.com/repositories/nightly/travisCI/$NAME.tar.gz
+  echo "wget https://www.arangodb.com/repositories/travisCI/$NAME.tar.gz"
+  wget https://www.arangodb.com/repositories/travisCI/$NAME.tar.gz
   echo "tar zxf $NAME.tar.gz"
   tar zvxf $NAME.tar.gz
 fi
