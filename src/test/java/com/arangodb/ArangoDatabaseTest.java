16462a10ff32151254f873f51f1072a41f2f87a2
mpv1989 <mark@arangodb.com>
2017-07-13 10:22:13 +0200

    Fix test

--- src/test/java/com/arangodb/ArangoDatabaseTest.java
+++ src/test/java/com/arangodb/ArangoDatabaseTest.java
@@ -691,7 +691,7 @@ public class ArangoDatabaseTest extends BaseTest {
 
 	@Test
 	public void queryWithWarning() {
-		final ArangoCursor<String> cursor = arangoDB.db().query("return _users + 1", null, null, String.class);
+		final ArangoCursor<String> cursor = arangoDB.db().query("return _apps + 1", null, null, String.class);
 
 		assertThat(cursor, is(notNullValue()));
 		assertThat(cursor.getWarnings(), is(notNullValue()));
