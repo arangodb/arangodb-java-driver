8dada8c023d0b0e1adab42aa212b259bb829963a
hkernbach <heiko.kernbach@gmail.com>
2019-07-18 12:14:51 +0200

    typo

--- src/main/java/com/arangodb/internal/velocystream/internal/VstConnection.java
+++ src/main/java/com/arangodb/internal/velocystream/internal/VstConnection.java
@@ -85,7 +85,7 @@ public abstract class VstConnection implements Connection {
 		this.sslContext = sslContext;
 		this.messageStore = messageStore;
 		
-		connectionName = "conenction_" + System.currentTimeMillis() + "_" + Math.random();
+		connectionName = "connection_" + System.currentTimeMillis() + "_" + Math.random();
 		LOGGER.debug("Connection " + connectionName + " created");
 	}
 
