8c11ab32f6b4fd2a5a0e5cadea3b224605870e0e
michele <michele@cheergg.com>
2019-09-11 14:07:56 +0200

    useTicketCache=true test

--- src/test/java/com/arangodb/BaseTest.java
+++ src/test/java/com/arangodb/BaseTest.java
@@ -55,12 +55,13 @@ public abstract class BaseTest {
         java.util.logging.Logger.getLogger("org.apache.http.headers").setLevel(java.util.logging.Level.FINEST);
         System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
         System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.headers", "debug");
-        System.setProperty("java.security.auth.login.config", "/home/michele/arango/arangodb-java-driver/src/test/resources/login.conf");
+        System.setProperty("java.security.auth.login.config", "/home/michele/arango/arangodb-java-driver/src/test/resources/login_keytab.conf");
+//        System.setProperty("java.security.auth.login.config", "/home/michele/arango/arangodb-java-driver/src/test/resources/login_cache.conf");
         System.setProperty("java.security.krb5.conf", "/etc/krb5.conf");
-//        System.setProperty("sun.security.krb5.debug", "true");
-//        System.setProperty("sun.security.jgss.debug", "true");
+        System.setProperty("sun.security.krb5.debug", "true");
+        System.setProperty("sun.security.jgss.debug", "true");
         System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
-
+        System.setProperty("sun.security.jgss.native", "true");
 
         if (arangoDB != null) {
             shutdown();
