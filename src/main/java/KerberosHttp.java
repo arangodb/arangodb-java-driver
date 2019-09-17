c1e146fceef84185a2e11b7db08f3114bb20dcb5
michele <michele@cheergg.com>
2019-09-17 12:42:38 +0200

    executable jar

--- src/main/java/KerberosHttp.java
+++ src/main/java/KerberosHttp.java
@@ -1,28 +1,10 @@
-package com.arangodb;/*
- * DISCLAIMER
- *
- * Copyright 2016 ArangoDB GmbH, Cologne, Germany
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Copyright holder is ArangoDB GmbH, Cologne, Germany
- */
-
+import com.arangodb.ArangoDB;
+import com.arangodb.Protocol;
 
 /**
  * @author Michele Rastelli
  */
-public class KerberosExpiration {
+public class KerberosHttp {
 
     public static void main(String[] args) throws InterruptedException {
         java.util.logging.Logger.getLogger("org.apache.http.headers").setLevel(java.util.logging.Level.FINEST);
@@ -35,10 +17,13 @@ public class KerberosExpiration {
         System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
         System.setProperty("sun.security.jgss.native", "true");
 
-        ArangoDB arangoDB = new ArangoDB.Builder().useProtocol(Protocol.HTTP_JSON).build();
+        ArangoDB arangoDB = new ArangoDB.Builder()
+                .host("bruecklinux.arangodb.biz", 8899)
+                .useProtocol(Protocol.HTTP_JSON)
+                .build();
 
         while (true) {
-            System.out.println(arangoDB.db("java_driver_test_db").getVersion().getVersion());
+            System.out.println(arangoDB.db().getVersion().getVersion());
             Thread.sleep(5000);
         }
     }
