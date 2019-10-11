54d9d91061df0f5bd408178c87c8958d659fffb6
michele <michele@cheergg.com>
2019-10-11 12:24:15 +0200

    ChunkStore refactoring

--- src/main/java/com/arangodb/internal/velocystream/internal/ChunkStore.java
+++ src/main/java/com/arangodb/internal/velocystream/internal/ChunkStore.java
@@ -20,6 +20,8 @@
 
 package com.arangodb.internal.velocystream.internal;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
@@ -27,45 +29,59 @@ import java.util.Map;
 
 /**
  * @author Mark Vollmary
- *
  */
 public class ChunkStore {
 
-	private final MessageStore messageStore;
-	private final Map<Long, ByteBuffer> data;
+    private final MessageStore messageStore;
+    private final Map<Long, ByteBuffer> data;
 
-	public ChunkStore(final MessageStore messageStore) {
-		super();
-		this.messageStore = messageStore;
-		data = new HashMap<>();
-	}
+    public ChunkStore(final MessageStore messageStore) {
+        super();
+        this.messageStore = messageStore;
+        data = new HashMap<>();
+    }
 
-	public ByteBuffer storeChunk(final Chunk chunk) throws BufferUnderflowException, IndexOutOfBoundsException {
-		final long messageId = chunk.getMessageId();
-		ByteBuffer chunkBuffer = data.get(messageId);
-		if (chunkBuffer == null) {
-			if (!chunk.isFirstChunk()) {
-				messageStore.cancel(messageId);
-				return null;
-			}
-			final int length = (int) (chunk.getMessageLength() > 0 ? chunk.getMessageLength()
-					: chunk.getContentLength());
-			chunkBuffer = ByteBuffer.allocate(length);
-			data.put(messageId, chunkBuffer);
-		}
-		return chunkBuffer;
-	}
+    public void storeChunk(final Chunk chunk, final InputStream inputStream) throws BufferUnderflowException, IndexOutOfBoundsException, IOException {
+        final long messageId = chunk.getMessageId();
+        ByteBuffer chunkBuffer = data.get(messageId);
+        if (chunkBuffer == null) {
+            if (!chunk.isFirstChunk()) {
+                messageStore.cancel(messageId);
+                return;
+            }
+            final int length = (int) (chunk.getMessageLength() > 0 ? chunk.getMessageLength()
+                    : chunk.getContentLength());
+            chunkBuffer = ByteBuffer.allocate(length);
+            data.put(messageId, chunkBuffer);
+        }
 
-	public void checkCompleteness(final long messageId) {
-		checkCompleteness(messageId, data.get(messageId));
-	}
+        final byte[] buf = new byte[chunk.getContentLength()];
+        readBytesIntoBuffer(inputStream, buf, 0, buf.length);
+        chunkBuffer.put(buf);
+        checkCompleteness(chunk.getMessageId());
+    }
 
-	private void checkCompleteness(final long messageId, final ByteBuffer chunkBuffer)
-			throws BufferUnderflowException, IndexOutOfBoundsException {
-		if (chunkBuffer.position() == chunkBuffer.limit()) {
-			messageStore.consume(new Message(messageId, chunkBuffer.array()));
-			data.remove(messageId);
-		}
-	}
+    private void readBytesIntoBuffer(final InputStream inputStream, final byte[] buf, final int off, final int len) throws IOException {
+        for (int readed = 0; readed < len; ) {
+            final int read = inputStream.read(buf, off + readed, len - readed);
+            if (read == -1) {
+                throw new IOException("Reached the end of the stream.");
+            } else {
+                readed += read;
+            }
+        }
+    }
+
+    private void checkCompleteness(final long messageId) {
+        checkCompleteness(messageId, data.get(messageId));
+    }
+
+    private void checkCompleteness(final long messageId, final ByteBuffer chunkBuffer)
+            throws BufferUnderflowException, IndexOutOfBoundsException {
+        if (chunkBuffer.position() == chunkBuffer.limit()) {
+            messageStore.consume(new Message(messageId, chunkBuffer.array()));
+            data.remove(messageId);
+        }
+    }
 
 }
