c1ebc15c7023e2a6819f9b23c1ba529cd0faf6c7
Michele Rastelli <rashtao@gmail.com>
2020-08-11 17:03:32 +0200

    updated versions of docker images

--- docker/start_db_cluster.sh
+++ docker/start_db_cluster.sh
@@ -5,7 +5,7 @@
 #   ./start_db_cluster.sh <dockerImage>
 
 # EXAMPLE:
-#   ./start_db_cluster.sh docker.io/arangodb/arangodb:3.6.1
+#   ./start_db_cluster.sh docker.io/arangodb/arangodb:3.7.1
 
 docker pull "$1"
 
--- docker/start_db_single.sh
+++ docker/start_db_single.sh
@@ -5,7 +5,7 @@
 #   ./start_db_single.sh <dockerImage>
 
 # EXAMPLE:
-#   ./start_db_single.sh docker.io/arangodb/arangodb:3.6.1
+#   ./start_db_single.sh docker.io/arangodb/arangodb:3.7.1
 
 docker pull "$1"
 
