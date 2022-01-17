fec7a8a4d9678140dbea871cb4988415bf078d03
Michele Rastelli <michele@arangodb.com>
2022-01-17 10:44:30 +0100

    AF: check host before closing

--- src/main/java/com/arangodb/internal/velocystream/VstCommunicationSync.java
+++ src/main/java/com/arangodb/internal/velocystream/VstCommunicationSync.java
@@ -151,8 +151,8 @@ public class VstCommunicationSync extends VstCommunication<Response, VstConnecti
             }
             final String location = e.getLocation();
             final HostDescription redirectHost = HostUtils.createFromLocation(location);
-            hostHandler.closeCurrentOnError();
-            hostHandler.fail(e);
+            hostHandler.closeCurrentOnErrorIfNotMatch(redirectHost);
+            hostHandler.failIfNotMatch(redirectHost, e);
             return execute(request, new HostHandle().setHost(redirectHost), attemptCount + 1);
         }
     }
