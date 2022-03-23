5cdf902a7fb7840ddced46ff13621eaaa99189c8
Michele Rastelli <michele@arangodb.com>
2022-03-23 14:21:23 +0100

    fixed ConcurrencyTests

--- src/test/java/com/arangodb/ConcurrencyTests.java
+++ src/test/java/com/arangodb/ConcurrencyTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.EnumSource;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -15,17 +16,16 @@ class ConcurrencyTests {
     @ParameterizedTest
     @EnumSource(Protocol.class)
     void concurrentPendingRequests(Protocol protocol) throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(10);
         ArangoDB adb = new ArangoDB.Builder().useProtocol(protocol).build();
         List<CompletableFuture<Void>> futures = IntStream.range(0, 10)
-                .mapToObj(i -> CompletableFuture.runAsync(
-                        () -> adb.db().query("RETURN SLEEP(1)", Void.class),
-                        Executors.newFixedThreadPool(10))
-                )
+                .mapToObj(__ -> CompletableFuture.runAsync(() -> adb.db().query("RETURN SLEEP(1)", Void.class), es))
                 .collect(Collectors.toList());
         for (CompletableFuture<Void> f : futures) {
             f.get();
         }
         adb.shutdown();
+        es.shutdown();
     }
 
 }
