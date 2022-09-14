87566e29099c1dc763711e3d1d8ce2605c208817
Michele Rastelli <michele@arangodb.com>
2022-09-14 14:28:06 +0200

    [DE-380] fixed ConsolidationPolicy API
    (cherry picked from commit e4c8b5c87917e7a54529a0a18e7fd897ae222957)

--- src/main/java/com/arangodb/entity/arangosearch/ConsolidationType.java
+++ src/main/java/com/arangodb/entity/arangosearch/ConsolidationType.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum ConsolidationType {
 
+    /**
+     * @deprecated The “bytes_accum” policy type is deprecated and remains in ArangoSearch for backwards compatibility
+     * with the older versions. Please make sure to always use the “tier” policy instead.
+     */
+    @Deprecated
     @JsonProperty("bytes_accum")
     BYTES_ACCUM,
 
