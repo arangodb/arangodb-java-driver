a7fb568b2a6a58b9c83c1f7fa1387f4d9083f404
Michele Rastelli <michele@arangodb.com>
2022-09-19 10:12:26 +0200

    [DE-384] added SearchAliasIndex operation type
    (cherry picked from commit fe0f79838c6fc6375038886c82bfb090ffcdfee0)

--- src/main/java/com/arangodb/entity/arangosearch/SearchAliasIndex.java
+++ src/main/java/com/arangodb/entity/arangosearch/SearchAliasIndex.java
@@ -10,11 +10,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class SearchAliasIndex {
     private final String collection;
     private final String index;
+    private final OperationType operation;
 
+    /**
+     * @param collection The name of a collection.
+     * @param index      The name of an inverted index of the collection.
+     */
+    public SearchAliasIndex(String collection, String index) {
+        this(collection, index, null);
+    }
+
+    /**
+     * @param collection The name of a collection.
+     * @param index      The name of an inverted index of the collection.
+     * @param operation  Whether to add or remove the index to the stored indexes property of the View. (default "add")
+     */
     @JsonCreator
-    public SearchAliasIndex(@JsonProperty("collection") String collection, @JsonProperty("index") String index) {
+    public SearchAliasIndex(
+            @JsonProperty("collection") String collection,
+            @JsonProperty("index") String index,
+            @JsonProperty("operation") OperationType operation) {
         this.collection = collection;
         this.index = index;
+        this.operation = operation;
     }
 
     public String getCollection() {
@@ -23,5 +41,13 @@ public class SearchAliasIndex {
 
     public String getIndex() {
         return index;
+    }
+
+    public OperationType getOperation() {
+        return operation;
+    }
+
+    public enum OperationType {
+        add, del
     }
 }
