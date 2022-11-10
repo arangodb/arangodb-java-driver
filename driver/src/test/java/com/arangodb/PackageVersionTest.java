3944b0e529faa2a47f532e5678ed6c6508fcde91
Michele Rastelli <michele@arangodb.com>
2022-11-10 12:14:35 +0100

    [DE-427] user agent

--- driver/src/test/java/com/arangodb/PackageVersionTest.java
+++ driver/src/test/java/com/arangodb/PackageVersionTest.java
@@ -1,11 +1,16 @@
 package com.arangodb;
 
+import com.arangodb.internal.config.FileConfigPropertiesProvider;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import ru.lanwen.verbalregex.VerbalExpression;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-class PackageVersionTest {
+class PackageVersionTest extends BaseJunit5 {
 
     @Test
     void packageVersion() {
@@ -23,5 +28,22 @@ class PackageVersionTest {
                 .endOfLine()
                 .build();
         assertThat(PackageVersion.VERSION).matches(testRegex.toString());
+    }
+
+    @ParameterizedTest
+    @EnumSource(Protocol.class)
+    void userAgentHeader(Protocol protocol) {
+        assumeTrue(protocol != Protocol.VST);
+        ArangoDB adb = new ArangoDB.Builder()
+                .loadProperties(new FileConfigPropertiesProvider())
+                .useProtocol(protocol)
+                .build();
+
+        Response<JsonNode> resp = adb.execute(Request.builder()
+                .method(Request.Method.GET)
+                .path("/_admin/echo")
+                .build(), JsonNode.class);
+        assertThat(resp.getBody().get("headers").get("user-agent").textValue()).endsWith(PackageVersion.VERSION);
+        adb.shutdown();
     }
 }
