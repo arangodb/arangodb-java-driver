44b1ad4fdfb0b6db7a3e07e602ec9e8dd662e6aa
Michele Rastelli <michele@arangodb.com>
2022-12-12 10:18:36 +0100

    shaded native tests

--- driver/src/test/java/com/arangodb/serde/SerdeTest.java
+++ driver/src/test/java/com/arangodb/serde/SerdeTest.java
@@ -5,13 +5,10 @@ import com.arangodb.internal.serde.InternalSerde;
 import com.arangodb.internal.serde.SerdeUtils;
 import com.arangodb.util.RawBytes;
 import com.arangodb.util.RawJson;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-
-import java.sql.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,34 +35,6 @@ class SerdeTest {
         byte[] serialized = s.serialize(raw);
         RawBytes deserialized = s.deserialize(serialized, RawBytes.class);
         assertThat(deserialized).isEqualTo(raw);
-    }
-
-    @ParameterizedTest
-    @EnumSource(ContentType.class)
-    void utilDateSerde(ContentType type) {
-        InternalSerde s = InternalSerde.of(type, null);
-        long ts = 1000000000000L;
-        java.util.Date date = new java.util.Date(ts);
-        byte[] ser = s.serialize(date);
-        JsonNode node = s.parse(ser);
-        assertThat(node.isLong()).isTrue();
-        assertThat(node.longValue()).isEqualTo(ts);
-        java.util.Date deser = s.deserialize(ser, java.util.Date.class);
-        assertThat(deser).isEqualTo(date);
-    }
-
-    @ParameterizedTest
-    @EnumSource(ContentType.class)
-    void sqlDateSerde(ContentType type) {
-        InternalSerde s = InternalSerde.of(type, null);
-        long ts = 1000000000000L;
-        java.sql.Date date = new Date(ts);
-        byte[] ser = s.serialize(date);
-        JsonNode node = s.parse(ser);
-        assertThat(node.isLong()).isTrue();
-        assertThat(node.longValue()).isEqualTo(ts);
-        java.sql.Date deser = s.deserialize(ser, java.sql.Date.class);
-        assertThat(deser).isEqualTo(date);
     }
 
 }
--- shaded-integration-tests/src/test/native/java/com/arangodb/serde/SerdeTest.java
+++ shaded-integration-tests/src/test/native/java/com/arangodb/serde/SerdeTest.java
@@ -3,15 +3,12 @@ package com.arangodb.serde;
 import com.arangodb.ContentType;
 import com.arangodb.internal.serde.InternalSerde;
 import com.arangodb.internal.serde.SerdeUtils;
-import com.arangodb.shaded.fasterxml.jackson.databind.JsonNode;
 import com.arangodb.shaded.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.arangodb.shaded.fasterxml.jackson.databind.node.ObjectNode;
 import com.arangodb.util.RawBytes;
 import com.arangodb.util.RawJson;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-
-import java.sql.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,34 +35,6 @@ class SerdeTest {
         byte[] serialized = s.serialize(raw);
         RawBytes deserialized = s.deserialize(serialized, RawBytes.class);
         assertThat(deserialized).isEqualTo(raw);
-    }
-
-    @ParameterizedTest
-    @EnumSource(ContentType.class)
-    void utilDateSerde(ContentType type) {
-        InternalSerde s = InternalSerde.of(type, null);
-        long ts = 1000000000000L;
-        java.util.Date date = new java.util.Date(ts);
-        byte[] ser = s.serialize(date);
-        JsonNode node = s.parse(ser);
-        assertThat(node.isLong()).isTrue();
-        assertThat(node.longValue()).isEqualTo(ts);
-        java.util.Date deser = s.deserialize(ser, java.util.Date.class);
-        assertThat(deser).isEqualTo(date);
-    }
-
-    @ParameterizedTest
-    @EnumSource(ContentType.class)
-    void sqlDateSerde(ContentType type) {
-        InternalSerde s = InternalSerde.of(type, null);
-        long ts = 1000000000000L;
-        Date date = new Date(ts);
-        byte[] ser = s.serialize(date);
-        JsonNode node = s.parse(ser);
-        assertThat(node.isLong()).isTrue();
-        assertThat(node.longValue()).isEqualTo(ts);
-        Date deser = s.deserialize(ser, Date.class);
-        assertThat(deser).isEqualTo(date);
     }
 
 }
