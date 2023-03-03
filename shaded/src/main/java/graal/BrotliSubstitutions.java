48b6673fd91e9288bdd4602a0007000b077b2f87
Michele Rastelli <michele@arangodb.com>
2023-03-03 20:37:31 +0100

    shaded native tests with --link-at-build-time

---
+++ shaded/src/main/java/graal/BrotliSubstitutions.java
@@ -0,0 +1,22 @@
+package graal;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+
+@TargetClass(className = "io.netty.handler.codec.compression.Brotli")
+final class Target_com_arangodb_shaded_netty_handler_codec_compression_Brotli {
+    @Substitute
+    public static boolean isAvailable() {
+        return false;
+    }
+
+    @Substitute
+    public static void ensureAvailability() throws Throwable {
+        throw new UnsupportedOperationException();
+    }
+}
+
+public class BrotliSubstitutions {
+
+}
---
+++ shaded/src/main/java/graal/EmptyByteBufStub.java
@@ -0,0 +1,33 @@
+package graal;
+
+import io.netty.util.internal.PlatformDependent;
+
+import java.nio.ByteBuffer;
+
+public final class EmptyByteBufStub {
+    private static final ByteBuffer EMPTY_BYTE_BUFFER = ByteBuffer.allocateDirect(0);
+    private static final long EMPTY_BYTE_BUFFER_ADDRESS;
+
+    static {
+        long emptyByteBufferAddress = 0;
+        try {
+            if (PlatformDependent.hasUnsafe()) {
+                emptyByteBufferAddress = PlatformDependent.directBufferAddress(EMPTY_BYTE_BUFFER);
+            }
+        } catch (Throwable t) {
+            // Ignore
+        }
+        EMPTY_BYTE_BUFFER_ADDRESS = emptyByteBufferAddress;
+    }
+
+    public static ByteBuffer emptyByteBuffer() {
+        return EMPTY_BYTE_BUFFER;
+    }
+
+    public static long emptyByteBufferAddress() {
+        return EMPTY_BYTE_BUFFER_ADDRESS;
+    }
+
+    private EmptyByteBufStub() {
+    }
+}
