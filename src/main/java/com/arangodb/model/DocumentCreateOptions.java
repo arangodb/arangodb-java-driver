9e620174e43be89b89a37a7fad0917781bceed7e
Michele Rastelli <michele@arangodb.com>
2023-04-25 11:59:15 +0200

    Index cache refilling (DE-564)

--- src/main/java/com/arangodb/model/DocumentCreateOptions.java
+++ src/main/java/com/arangodb/model/DocumentCreateOptions.java
@@ -36,6 +36,7 @@ public class DocumentCreateOptions {
     private Boolean silent;
     private String streamTransactionId;
     private Boolean mergeObjects;
+    private Boolean refillIndexCaches;
 
 
     public DocumentCreateOptions() {
@@ -168,4 +169,17 @@ public class DocumentCreateOptions {
         return this;
     }
 
+    public Boolean getRefillIndexCaches() {
+        return refillIndexCaches;
+    }
+
+    /**
+     * @param refillIndexCaches Whether to add a new entry to the in-memory edge cache if an edge document is inserted.
+     * @return options
+     * @since ArangoDB 3.11
+     */
+    public DocumentCreateOptions refillIndexCaches(Boolean refillIndexCaches) {
+        this.refillIndexCaches = refillIndexCaches;
+        return this;
+    }
 }
