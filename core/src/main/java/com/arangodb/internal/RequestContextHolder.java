5af59b13442f55068b6aa20252e9948eafa6b4e4
Michele Rastelli <michele@arangodb.com>
2024-04-10 14:41:03 +0200

    RequestContextHolder: support re-entrant invocations

--- core/src/main/java/com/arangodb/internal/RequestContextHolder.java
+++ core/src/main/java/com/arangodb/internal/RequestContextHolder.java
@@ -4,6 +4,7 @@ import com.arangodb.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 
 public enum RequestContextHolder {
@@ -11,27 +12,31 @@ public enum RequestContextHolder {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(RequestContextHolder.class);
 
-    private final ThreadLocal<RequestContext> ctx = ThreadLocal.withInitial(() -> RequestContext.EMPTY);
     private final ThreadLocal<Boolean> runningWithinCtx = ThreadLocal.withInitial(() -> false);
+    private final ThreadLocal<RequestContext> ctx = ThreadLocal.withInitial(() -> RequestContext.EMPTY);
 
     public <T> T runWithCtx(RequestContext requestContext, Supplier<T> fun) {
+        Objects.requireNonNull(requestContext);
+        RequestContext old = null;
         try {
             if (runningWithinCtx.get()) {
-                throw new IllegalStateException("re-entrant invocation is not supported");
+                // re-entrant invocation, keep track of old ctx to restore later
+                old = ctx.get();
             }
-
+            LOGGER.debug("setting RequestContext: {}", requestContext);
+            ctx.set(requestContext);
             runningWithinCtx.set(true);
-
-            if (requestContext != null) {
-                LOGGER.debug("setting RequestContext: {}", requestContext);
-                ctx.set(requestContext);
-            }
-
             return fun.get();
         } finally {
-            LOGGER.debug("removing RequestContext");
-            ctx.remove();
-            runningWithinCtx.remove();
+            if (old == null) {
+                LOGGER.debug("removing RequestContext");
+                ctx.remove();
+                runningWithinCtx.remove();
+            } else {
+                // re-entrant invocation, restore old ctx
+                LOGGER.debug("restore RequestContext: {}", old);
+                ctx.set(old);
+            }
         }
     }
 
