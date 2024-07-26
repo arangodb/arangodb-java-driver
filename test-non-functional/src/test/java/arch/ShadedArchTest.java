7eec5420261579404e71bf3a30b946c36a44aa8e
Michele Rastelli <michele@arangodb.com>
2024-07-26 12:18:39 +0200

    re-enabled shaded arch tests

--- test-non-functional/src/test/java/arch/ShadedArchTest.java
+++ test-non-functional/src/test/java/arch/ShadedArchTest.java
@@ -1,47 +1,87 @@
-// FIXME
-//package arch;
-//
-//import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
-//import com.tngtech.archunit.junit.AnalyzeClasses;
-//import com.tngtech.archunit.junit.ArchTest;
-//import com.tngtech.archunit.lang.ArchRule;
-//
-//import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
-//
-//
-//@AnalyzeClasses(packages = "com.arangodb..", importOptions = {DoNotIncludeTests.class})
-//public class ShadedArchTest {
-//
-//    @ArchTest
-//    public static final ArchRule nettyRelocation = noClasses().that()
-//            .resideInAPackage("com.arangodb..")
-//            .should().dependOnClassesThat()
-//            .resideInAPackage("io.netty..");
-//
-//    @ArchTest
-//    public static final ArchRule vertxRelocation = noClasses().that()
-//            .resideInAPackage("com.arangodb..")
-//            .should().dependOnClassesThat()
-//            .resideInAPackage("io.vertx..");
-//
-//    @ArchTest
-//    public static final ArchRule jacksonRelocation = noClasses().that()
-//            .resideInAPackage("com.arangodb..").and()
-//            .resideOutsideOfPackage("com.arangodb.jackson.dataformat.velocypack..").and()
-//            .resideOutsideOfPackage("com.arangodb.serde.jackson..")
-//            .should().dependOnClassesThat()
-//            .resideInAPackage("com.fasterxml.jackson..");
-//
-//    @ArchTest
-//    public static final ArchRule noJacksonDependency = noClasses().that()
-//            .resideInAPackage("com.arangodb..")
-//            .should().dependOnClassesThat()
-//            .resideInAPackage("com.fasterxml.jackson..");
-//
-//    @ArchTest
-//    public static final ArchRule noJacksonDataformatVelocypackDependency = noClasses().that()
-//            .resideInAPackage("com.arangodb..")
-//            .should().dependOnClassesThat()
-//            .resideInAPackage("com.arangodb.jackson.dataformat.velocypack..");
-//
-//}
+package arch;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.core.importer.ImportOption.Predefined.DO_NOT_INCLUDE_TESTS;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+
+public class ShadedArchTest {
+    private final JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.arangodb..");
+
+    private final boolean shaded = isShaded();
+
+    private static boolean isShaded() {
+        boolean shaded;
+        try {
+            Class.forName("com.arangodb.shaded.fasterxml.jackson.databind.JsonNode");
+            shaded = true;
+        } catch (ClassNotFoundException e) {
+            shaded = false;
+        }
+        return shaded;
+    }
+
+    @BeforeEach
+    void checkShaded() {
+        assumeTrue(shaded, "not shaded driver");
+    }
+
+    @Test
+    public void nettyRelocation() {
+        noClasses().that()
+                .resideInAPackage("com.arangodb..")
+                .should().dependOnClassesThat()
+                .resideInAPackage("io.netty..")
+                .check(importedClasses);
+    }
+
+    @Test
+    public void vertxRelocation() {
+        noClasses().that()
+                .resideInAPackage("com.arangodb..")
+                .should().dependOnClassesThat()
+                .resideInAPackage("io.vertx..")
+                .check(importedClasses);
+    }
+
+    @Test
+    public void jacksonRelocation() {
+        noClasses().that()
+                .resideInAPackage("com.arangodb..").and()
+                .resideOutsideOfPackage("com.arangodb.jackson.dataformat.velocypack..").and()
+                .resideOutsideOfPackage("com.arangodb.serde.jackson..")
+                .should().dependOnClassesThat()
+                .resideInAPackage("com.fasterxml.jackson..")
+                .check(importedClasses);
+    }
+
+    @Test
+    public void noJacksonDependency() {
+        noClasses().that()
+                .resideInAPackage("com.arangodb..").and()
+                .resideOutsideOfPackages(
+                        "com.arangodb.jackson.dataformat.velocypack..",
+                        "com.arangodb.serde.jackson..")
+                .should().dependOnClassesThat()
+                .resideInAPackage("com.fasterxml.jackson..")
+                .check(importedClasses);
+    }
+
+    @Test
+    public void noJacksonDataformatVelocypackDependency() {
+        noClasses().that()
+                .resideInAPackage("com.arangodb..").and()
+                .resideOutsideOfPackage("com.arangodb.jackson.dataformat.velocypack..")
+                .should().dependOnClassesThat()
+                .resideInAPackage("com.arangodb.jackson.dataformat.velocypack..")
+                .check(importedClasses);
+    }
+
+}
