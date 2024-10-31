7c29d5811c87e4f59a89cffa9495419d32c35962
Michele Rastelli <michele@arangodb.com>
2024-10-31 14:25:45 +0100

    CI: fix port mapping

--- docker/start_proxy.sh
+++ docker/start_proxy.sh
@@ -4,5 +4,5 @@ docker run -d \
   -e LOG_LEVEL=Info \
   -e AUTH_USER=user \
   -e AUTH_PASSWORD=password \
-  --network=arangodb --ip=172.28.0.100 \
+  --network=arangodb -p 8888:8888 \
   docker.io/kalaksi/tinyproxy:1.7
--- test-functional/src/test-ssl/java/com/arangodb/ArangoSslTest.java
+++ test-functional/src/test-ssl/java/com/arangodb/ArangoSslTest.java
@@ -56,7 +56,7 @@ class ArangoSslTest extends BaseTest {
                 .protocolConfig(HttpProtocolConfig.builder()
                         .proxyOptions(new ProxyOptions()
                                 .setType(ProxyType.HTTP)
-                                .setHost("172.28.0.100")
+                                .setHost("172.28.0.1")
                                 .setPort(8888)
                                 .setUsername("user")
                                 .setPassword("password"))
