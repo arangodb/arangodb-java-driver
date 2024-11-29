8b1c62ae1672abf959963d9ee2501a9c5afe4da8
Michele Rastelli <michele@arangodb.com>
2024-11-29 12:28:40 +0100

    improved serialization of JsonNode by avoid intermediate parsing when not needed

--- core/src/main/java/com/arangodb/internal/serde/UserDataSerializer.java
+++ core/src/main/java/com/arangodb/internal/serde/UserDataSerializer.java
@@ -2,6 +2,7 @@ package com.arangodb.internal.serde;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
@@ -16,10 +17,14 @@ class UserDataSerializer extends JsonSerializer<Object> {
 
     @Override
     public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        // TODO: find a way to append raw bytes directly
-        // see https://github.com/FasterXML/jackson-core/issues/914
-        try (JsonParser parser = gen.getCodec().getFactory().createParser(serde.serializeUserData(value))) {
-            gen.writeTree(parser.readValueAsTree());
+        if (value != null && JsonNode.class.isAssignableFrom(value.getClass())) {
+            gen.writeTree((JsonNode) value);
+        } else {
+            // TODO: find a way to append raw bytes directly
+            // see https://github.com/FasterXML/jackson-core/issues/914
+            try (JsonParser parser = gen.getCodec().getFactory().createParser(serde.serializeUserData(value))) {
+                gen.writeTree(parser.readValueAsTree());
+            }
         }
     }
 }
