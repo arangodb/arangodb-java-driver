fcc178ae59774532351ef2095a094903914b007a
Michele Rastelli <michele@arangodb.com>
2026-07-09 12:33:14 +0200

    CI: pin tinyproxy docker image to digest for reproducibility

--- docker/start_proxy.sh
+++ docker/start_proxy.sh
@@ -5,4 +5,4 @@ docker run -d \
   -e AUTH_USER=user \
   -e AUTH_PASSWORD=password \
   --network=arangodb -p 8888:8888 \
-  docker.io/kalaksi/tinyproxy:1.7
+  docker.io/kalaksi/tinyproxy@sha256:6eddb7eba70227000b2a8948e84ecbf88db87bc910a54682ebef58cef9eb3887
